efa8686ba6e0f294f162fb6e7128ed69feae79d4
Angie <angiepolitte@gmail.com>
2024-12-17 21:29:45 -0600

    Exercises annotations and validation with errors

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,6 +2,8 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
@@ -25,5 +27,16 @@ public class EventController {
         model.addAttribute("events", events);
         return "events/index";
     }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        return "edit/{eventId}";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        return "edit";
+    }
+
 
 }
