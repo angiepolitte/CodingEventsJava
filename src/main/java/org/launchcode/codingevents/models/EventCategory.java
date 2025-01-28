a7bcd1a842b81383f5ae43886992a9e51b673d19
Angie <angiepolitte@gmail.com>
2025-01-28 14:30:00 -0600

    Coursework with Chris

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,21 +1,21 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Size;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
 
-//    @Id
-//    @GeneratedValue
-//    private int id;
 
     @Size(min=3, message = "Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long")String name) {
         this.name = name;
@@ -23,9 +23,7 @@ public class EventCategory extends AbstractEntity {
 
     public EventCategory() {};
 
-//    public int getId() {
-//        return id;
-//    }
+
 
     public String getName() {
         return name;
@@ -39,17 +37,8 @@ public class EventCategory extends AbstractEntity {
     public String toString() {
         return name;
     }
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        EventCategory that = (EventCategory) o;
-//        return id == that.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hashCode(id);
-//    }
+//no setter because it's final and cannot change
+    public List<Event> getEvents() {
+        return events;
+    }
 }
