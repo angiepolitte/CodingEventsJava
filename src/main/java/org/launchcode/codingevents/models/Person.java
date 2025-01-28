7bbb52235126e5cfb33999e90f11d8788fe82de1
Angie <angiepolitte@gmail.com>
2025-01-28 14:58:00 -0600

    Person class created

---
+++ src/main/java/org/launchcode/codingevents/models/Person.java
@@ -0,0 +1,17 @@
+package org.launchcode.codingevents.models;
+
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    //List of Events List<Event> @ManyToMany
+    //Favorite category EventCategory @OneToMany
+
+
+    //Methods
+    //-getters and setters
+    //-get back Favorite Events(returns all events that have to do with favorite category
+}
